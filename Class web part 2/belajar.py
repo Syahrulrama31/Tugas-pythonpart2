b89332113763db47cab24316599bc85031fe9573
Syahrulrama31 <113223383+Syahrulrama31@users.noreply.github.com>
2022-09-10 10:27:03 +0700

    Add files via upload

---
+++ Class web part 2/belajar.py
@@ -0,0 +1,34 @@
+from ast import If
+from operator import truediv
+
+
+def fungsi_python():
+    print("Hello gaes")
+
+
+# fungsi_python()
+# Ini ptyhon
+nama = "Syahrul Rama"
+umur = 19
+ukuransepatu = 40.1
+print("Nama saya adalah " + nama + "umur saya " + str(umur))
+print("Umur saya adalah : ", umur)
+print("ukuran sepatu saya :", ukuransepatu, "dia adalah", type(ukuransepatu))
+
+# list
+hewan = ["ayam", "kucing", "kelinci", "sapi"]
+# hewanlaut= ("hiu","paus","ubur-ubur")
+# nomor={"1","2","3"}
+print(hewan[1])
+
+x = 6
+y = 2
+a = 10
+print("penjumlahan: " + str(x+y+a))
+print("pengurangan: " + str(x-y+-a))
+print("pembagian: " + str(a/y))
+print("perkalian: " + str(x*y*a))
+print("modulus: " + str(75 % 10))
+
+
+
